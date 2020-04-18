78670698a8f762b34d199aabb7ab365b38a4e63d
vedantpople <vedantpople4@gmail.com>
2020-04-18 14:43:00 +0530

    Creating Stateless Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
