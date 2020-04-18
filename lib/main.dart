a1b7c40354b312f6638452642d983e99eaf462ba
vedantpople <vedantpople4@gmail.com>
2020-04-18 15:04:08 +0530

    Changing the Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: Scaffold(
+        backgroundColor: Colors.tealAccent,
+        body: Container(),
+      ),
     );
   }
 }
