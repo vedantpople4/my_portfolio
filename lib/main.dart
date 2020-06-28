f7de6e5b960937757d16ed20cd48fd7c30c5c4a7
vedantpople4 <vedantpople4@gmial.com>
2020-06-28 16:41:53 +0530

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class HomePage extends StatelessWidget{
               Text(
                   'Vedant Pople',
                   style: TextStyle(
-                  fontSize: .0,
+                  fontSize: 35.0,
                   color: Colors.black,
                   fontWeight: FontWeight.bold,
                   fontStyle: FontStyle.italic,
