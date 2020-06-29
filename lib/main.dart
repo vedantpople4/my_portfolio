60728154399e3daa2e9f1195470fef914dfa23ee
vedantpople <vedantpople4@gmail.com>
2020-06-29 13:37:38 +0530

    New change
    New Change

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'My Portfolio App',
+        title: 'MyApp',
         home: new HomePage(),
         initialRoute: '/',
         routes: <String, WidgetBuilder>{
