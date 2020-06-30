e0b954e8ae95bb45128a6081722d61671b062e6f
vedantpople <vedantpople4@gmail.com>
2020-06-30 15:09:19 +0530

    new

--- lib/datascience.dart
+++ lib/datascience.dart
@@ -3,53 +3,50 @@ import 'package:flutter/material.dart';
 import 'package:mycard/main.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-launchlogin() async  {
+launchlogin() async {
   const url = 'https://github.com/vedantpople4/Login-and-registration';
-  if(canLaunch(url) != null){
+  if (canLaunch(url) != null) {
     launch(url);
-  }
-  else{
+  } else {
     throw 'Could not launch $url';
   }
-}dekho we awill rock you is waht we said 
+}
 
 lauchcalc() async {
   const url = 'https://github.com/vedantpople4/flutter_calculator';
-  if(canLaunch(url)!=null){
+  if (canLaunch(url) != null) {
     launch(url);
-  }
-  else{
+  } else {
     throw 'Could no launch $url';
   }
 }
 
-launchbasic() async{
-  const url ='https://github.com/vedantpople4/Basic_ML_projects';
-  if(canLaunch(url)!=null){
+launchbasic() async {
+  const url = 'https://github.com/vedantpople4/Basic_ML_projects';
+  if (canLaunch(url) != null) {
     launch(url);
-  }else{
+  } else {
     throw 'Could not launch $url';
   }
 }
 
-launchml() async{
+launchml() async {
   const url = 'https://github.com/vedantpople4/ML_Projects';
-  if(canLaunch(url)!=null){
-    launch(url);}
-    else{
-      throw 'Could not launch $url';
+  if (canLaunch(url) != null) {
+    launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
 
-
 class DataScience extends MyApp {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Data Science',
       home: Scaffold(
         backgroundColor: Colors.white,
-        appBar:AppBar (
+        appBar: AppBar(
           title: Text('Projects Page'),
           backgroundColor: Colors.black,
         ),
@@ -64,27 +61,29 @@ class DataScience extends MyApp {
                 child: Column(
                   children: <Widget>[
                     Text('This project uses image of a user for registration.'),
-                    Text('This helps the admin manage for maintainable purposes.'),
+                    Text(
+                        'This helps the admin manage for maintainable purposes.'),
                   ],
                 ),
               ),
               FlatButton(
                 child: Text('Login Registration'),
-                onPressed: ()=>launchlogin(),
+                onPressed: () => launchlogin(),
               ),
               Card(
-                elevation : 0,
+                elevation: 0,
                 child: Column(
                   children: <Widget>[
                     Text('This project is a calculator App.'),
-                    Text('This app can do addition, subtraction, multiplication '),
+                    Text(
+                        'This app can do addition, subtraction, multiplication '),
                     Text('and division of two numbers. '),
                   ],
                 ),
               ),
               FlatButton(
                 child: Text('Calculator App'),
-                onPressed: ()=> lauchcalc(),
+                onPressed: () => lauchcalc(),
               ),
               Card(
                 elevation: 0,
@@ -97,7 +96,7 @@ class DataScience extends MyApp {
               ),
               FlatButton(
                 child: Text('Basic ML Projects'),
-                onPressed: ()=>launchbasic(),
+                onPressed: () => launchbasic(),
               ),
               Card(
                 elevation: 0,
@@ -110,20 +109,20 @@ class DataScience extends MyApp {
               ),
               FlatButton(
                 child: Text('ML Projects'),
-                onPressed: ()=>launchml(),
+                onPressed: () => launchml(),
               ),
               Card(
                 elevation: 0,
-                child : Column(
-                    children :<Widget> [
-                      Text('Dart APIs'),
-                      Text('Various APIs made using Dart Frameworks'),
-                    ], 
+                child: Column(
+                  children: <Widget>[
+                    Text('Dart APIs'),
+                    Text('Various APIs made using Dart Frameworks'),
+                  ],
                 ),
               ),
               FlatButton(
                 child: Text('Dart APIs'),
-                onPressed: (){},
+                onPressed: () {},
               ),
             ],
           ),
